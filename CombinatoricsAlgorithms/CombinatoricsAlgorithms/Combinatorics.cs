297a148012bf1ab255548c31c7cb98e30aa719e2
Alessandro <gizz.bellissimo@yandex.ru>
2017-02-13 19:05:25 +0300

    Проект CombinatoricsAlgorithms добавлен в репозиторий

---
+++ CombinatoricsAlgorithms/CombinatoricsAlgorithms/Combinatorics.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+namespace TestCombinatorics
+{
+    public static class Combinatorics
+    {
+        public static List<string> PlacementsWithoutRepetition(int[] A)
+        {
+            int N = A.Length;
+            List<string> s = new List<string>();
+            for (int i = 0; i < N; i++)
+                for (int j = 0; j < N; j++)
+                {
+                    if (i == j) continue;
+                    s.Add(A[i] + " " + A[j]);
+                }
+            return s;
+        }
+
+        public static List<string> PlacementsWithRepetition(int[] A)
+        {
+            int N = A.Length;
+            List<string> s = new List<string>();
+            for (int i = 0; i < N; i++)
+                for (int j = 0; j < N; j++)
+                {
+                    s.Add(A[i] + " " + A[j]);
+                }
+            return s;
+        }
+
+        public static List<string> CombinationsWithoutRepetition(int[] A)
+        {
+            int N = A.Length;
+            List<string> s = new List<string>();
+            for (int i = 0; i < N - 1; i++)
+                for (int j = i + 1; j < N; j++)
+                {
+                    if (i != j) s.Add(A[i] + " " + A[j]);
+                }
+            return s;
+        }
+
+        public static List<string> CombinationsWithRepetition(int[] A)
+        {
+            int N = A.Length;
+            List<string> s = new List<string>();
+            for (int i = 0; i < N; i++)
+                for (int j = i; j < N; j++)
+                {
+                    s.Add(A[i] + " " + A[j]);
+                }
+            return s;
+        }
+    }
+}
---
+++ CombinatoricsAlgorithms/CombinatoricsAlgorithms/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestCombinatorics
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] A = { 1, 2, 3, 4, 5 };
+            List<string> S = Combinatorics.CombinationsWithoutRepetition(A);
+            foreach (string s in S)
+                Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
